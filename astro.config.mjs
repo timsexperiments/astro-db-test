1a81a4047bbbffc9862144ac7bee51d8a30f4dbd
Tim <hello@timsexperiments.foo>
2024-04-17 21:54:14 -0500

    add logic

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,14 @@
 import { defineConfig } from 'astro/config';
+import tailwind from "@astrojs/tailwind";
+import node from "@astrojs/node";
+
+import db from "@astrojs/db";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [tailwind(), db()],
+  output: "server",
+  adapter: node({
+    mode: "standalone"
+  })
+});
